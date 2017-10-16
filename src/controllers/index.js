afdfb1d9b10989a0c141fb9ae85055ea93c28da6
Jacky Jiang <t1000@Jings-MacBook-Air.local>
2017-10-16 14:02:00 +1100

    Add start time & end time

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,7 @@ import views from "co-views";
 // import parse from "co-body";
 import instantTheme from "koa-instant-theme";
 import db from "../db";
+import moment from "moment";
 
 const render = views(path.resolve(__dirname, "../views"), {
     map: { html: "ejs" },
@@ -12,6 +13,11 @@ export default async function index(ctx) {
     const records = await db.execute("SELECT * FROM images ORDER BY timestamp ASC").spread((records) => {
         return records;
     });
-    const body = await render("index", { records, recordsJSON:JSON.stringify(records) });
+    const body = await render("index", { 
+        records,
+        startDate: moment(records[0]['timestamp']).format("DD/MM/YYYY HH:mm:ss"),
+        endDate: moment(records[records.length-1]['timestamp']).format("DD/MM/YYYY HH:mm:ss"),
+        recordsJSON:JSON.stringify(records),
+    });
     ctx.body = instantTheme.header + body + instantTheme.footer;
 }
